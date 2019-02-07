0e50545b1606e1860e196b6d3a7676cbd304b3ed
ziweidream <ziwei.lotus@gmail.com>
2019-02-07 11:05:06 -0500

    Updates

---
+++ precache-manifest.2a86fe1542a11d7065ec1dfe1c2b2e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d9c9d424cf4ae94211",
+    "url": "/token-sale-react/static/js/runtime~main.14d9c9d4.js"
+  },
+  {
+    "revision": "311b3dc525d4787ce438",
+    "url": "/token-sale-react/static/js/main.311b3dc5.chunk.js"
+  },
+  {
+    "revision": "8bfd524bbedde7752733",
+    "url": "/token-sale-react/static/js/1.8bfd524b.chunk.js"
+  },
+  {
+    "revision": "311b3dc525d4787ce438",
+    "url": "/token-sale-react/static/css/main.b3f0e364.chunk.css"
+  },
+  {
+    "revision": "ad1d06af90281f6c733d0d81a8fabb52",
+    "url": "/token-sale-react/index.html"
+  }
+];
